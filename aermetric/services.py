711858c9ca0975872f7ae653096025bb9e2c9634
emilrakaev <emilrakaev@gmail.com>
2022-05-30 21:01:10 +0600

    Implemented error handling in UploadFileService

--- aermetric/services.py
+++ aermetric/services.py
@@ -1,46 +1,53 @@
+import csv
+from itertools import chain
+
 from django.core.files.base import ContentFile
 from django.core.files.storage import FileSystemStorage
-from aermetric.models import AircraftErrorData
-import csv
+from django.db.models import Sum, Count, Q
+
+from aermetric.exceptions import FileParseException, GenerateStatisticException
+from aermetric.models import AircraftStatData
 
 
 class UploadFileService:
     @classmethod
     def parse_file_and_upload_in_model(cls, file):
-        fs = FileSystemStorage(location='tmp/')
+        try:
+            fs = FileSystemStorage(location='tmp/')
 
-        content = file.read()
-        file_content = ContentFile(content)
-        file_name = fs.save(
-            "_tmp.csv", file_content
-        )
-        tmp_file = fs.path(file_name)
-
-        csv_file = open(tmp_file, errors="ignore")
-        reader = csv.reader(csv_file)
-        next(reader)
-
-        product_list = []
-
-        for id_, row in enumerate(reader):
-            (
-                priority,
-                type,
-                aircraft,
-                status,
-                errors_count,
-                info_count
-            ) = row
-
-            product_list.append(
-                AircraftErrorData(
-                    priority=priority,
-                    type=type,
-                    aircraft=aircraft,
-                    status=status,
-                    errors_count=errors_count,
-                    info_count=info_count,
-                )
+            content = file.read()
+            file_content = ContentFile(content)
+            file_name = fs.save(
+                "_tmp.csv", file_content
             )
+            tmp_file = fs.path(file_name)
 
-        AircraftErrorData.objects.bulk_create(product_list)
+            csv_file = open(tmp_file, errors="ignore")
+            reader = csv.reader(csv_file)
+            next(reader)
+            data_list = []
+
+            for id_, row in enumerate(reader):
+                (
+                    priority,
+                    type,
+                    aircraft,
+                    status,
+                    errors_count,
+                    info_count
+                ) = row
+
+                data_list.append(
+                    AircraftStatData(
+                        priority=priority,
+                        type=type,
+                        aircraft=aircraft,
+                        status=status,
+                        errors_count=errors_count,
+                        info_count=info_count,
+                    )
+                )
+
+            AircraftStatData.objects.bulk_create(data_list)
+        except Exception as e:
+            raise FileParseException(f'Something wrong with upload file, error: {e}')
--- aermetric/views.py
+++ aermetric/views.py
@@ -1,17 +1,18 @@
+from rest_framework import status as rest_status, status
 from rest_framework.generics import GenericAPIView
-
 from rest_framework.response import Response
-from rest_framework import status
 
-from aermetric.serializers import FileFieldSerializer
+from aermetric.serializers import AircraftStatDataSerializer
 from aermetric.services import UploadFileService
 
 
 class UploadData(GenericAPIView):
-
     def post(self, request, *args, **kwargs):
-        file = request.FILES['file']
+        try:
+            file = request.FILES['file']
+            UploadFileService.parse_file_and_upload_in_model(file)
+        except Exception as e:
+            return Response(data={f"Error": f"{str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
 
-        UploadFileService.parse_file_and_upload_in_model(file)
+        return Response("Successfully upload the data", status=rest_status.HTTP_201_CREATED)
 
-        return Response("Successfully upload the data", status=status.HTTP_201_CREATED)
