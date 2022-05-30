c0f98f820e237d6f61e3d9b7fdff53f09b0c25a7
emilrakaev <emilrakaev@gmail.com>
2022-05-30 20:55:39 +0600

    Register AircraftStatData model in admin

--- aermetric/admin.py
+++ aermetric/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from aermetric.models import AircraftErrorData
+from aermetric.models import AircraftStatData
 
 
 class AircraftErrorDataAdmin(admin.ModelAdmin):
     list_display = ["id", "priority", "type", "aircraft", "status", "errors_count", "info_count"]
 
 
-admin.site.register(AircraftErrorData, AircraftErrorDataAdmin)
+admin.site.register(AircraftStatData, AircraftErrorDataAdmin)
